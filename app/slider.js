9a3a562fb771650d3c2e5ff1fc09057e6618d20b
Roman <Solohor.Roman@gmail.com>
2018-06-29 19:43:02 +0300

    slider for review section, footer and form

---
+++ app/slider.js
@@ -0,0 +1,31 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  } 
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  } 
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  slides[slideIndex-1].style.display = "block"; 
+
+}
