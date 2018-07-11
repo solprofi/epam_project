3493527201cb83fccfb68e1e3e2d9b1f128d45e1
Roman <Solohor.Roman@gmail.com>
2018-07-11 15:15:37 +0300

    Added a working navigation menu for index page

--- app/player.js
+++ app/player.js
@@ -21,19 +21,57 @@ var songTitles = [
 ]
 var poster = ["./assets/images/player-album-bg.jpeg","Poster2.jpg","Poster3.jpg"];
 
+
+// function getDuration(src, cb) {
+//     var audio = new Audio();
+//     $(audio).on("loadedmetadata", function(){
+//         cb(audio.duration);
+//     });
+//     audio.src = src;
+// }
+
+// var songLengths = songs.map( el => getDuration(el, (length) => length));
+// console.log(songLengths);
+
+
+// getDuration("./assets/music/Tuscan Leather.mp3", function(length) {
+//     console.log('I got length ' + length);
+// });
+
+
 var songTitle = document.getElementById("songTitle");
 var fillBar = document.getElementById("fill");
+var tracksList = document.getElementsByClassName('track-list__track');
+
+var prevSong = 0; //previous selected song
 
 var song = new Audio();
+
 var currentSong = 0;    // it point to the current song
 
-window.onload = playSong;   // it will call the function playSong when window is load
+window.onload = initialize;   // it will call the function playSong when window is load
 
 function pickSong(i) {
     currentSong = i;
+    updateListAppearance(i);
     playSong();  
     changeImgToPause();
 }
+
+function initialize() {
+    updateListAppearance(0);
+    playSong();
+}
+
+function updateListAppearance(i) {
+    tracksList[prevSong].style.fontWeight = "normal";
+    tracksList[prevSong].style.textTransform = "capitalize";
+
+    tracksList[i].style.fontWeight = "700";
+    tracksList[i].style.textTransform = "uppercase";
+    prevSong = i;
+}
+
 
 function playSong(){
     song.src = songs[currentSong];  //set the source of 0th song 
@@ -48,6 +86,8 @@ function changeImgToPause() {
 function changeImgToPlay() {
     $("#play img").attr("src", "./assets/images/Play.png");
 }
+
+
 
 
 function playOrPauseSong(){
@@ -69,6 +109,20 @@ song.addEventListener('timeupdate', function(){
     fillBar.style.width = position * 100 +'%';
 });
 
+document.addEventListener('keyup', function (e) {
+    var key = e.which || e.keyCode;
+    
+    if(key === 32) {
+        playOrPauseSong();
+    } else if (key === 37) {
+        pre();
+    } else if (key === 39) {
+        next();
+    }
+    
+});
+
+
 
 function next(){
     currentSong++;
@@ -77,6 +131,7 @@ function next(){
     }
     playSong();
     changeImgToPause();
+    updateListAppearance(currentSong);
     $("#image img").attr("src",poster[currentSong]);
 }
 
@@ -87,6 +142,7 @@ function pre(){
         currentSong = songs.length - 1;
     }
     playSong();
-    changeImgToPlay();
+    changeImgToPause();
+    updateListAppearance(currentSong);
     $("#image img").attr("src",poster[currentSong]);
 }
